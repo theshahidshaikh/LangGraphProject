a22cfee178a10033ba254194cdcb91883e3c4759
Shahid Shaikh <shahidshaikh3332@gmail.com>
2026-01-16 16:50:48 +0530

    Initial commit

--- ChatBot.py
+++ ChatBot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from langgraph.graph import StateGraph
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key='AIzaSyA7SfNkAmlWNwMJeunKR3rWzN4mrjHk5Bg')
+client = genai.Client(api_key='Your API Key')
 
 
 def chat(propmt):
